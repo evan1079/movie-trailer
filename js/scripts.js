da94a14a9658857437040deb16f2d641611ac866
evan1079 <59583051+evan1079@users.noreply.github.com>
2020-01-29 14:04:01 -0800

    Applied CodePen
    Applied HTML, CSS, and JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-console.log('OK');
+console.log('We are A-OK');
+
+//Initialize Player (Plyr)
+
+// https://cdnjs.cloudflare.com/ajax/libs/plyr/3.5.6/plyr.min.js
+
+const player = new Plyr( '#player', {
+  
+    controls : ['play', 'current-time', 'progress'], 
+    
+    keyboard : {focused: true, global: true},
+    
+    seekTime : 30,
+    
+    autoplay : true
+    
+  });
+  
+  console.log( player );
